b7e5a8da5aa6566ff911ebb22d58ef00316f3802
rushikeshpatil1007 <patilrushikesh1007@gmail.com>
2023-10-11 17:06:27 +0530

    client

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <div className="container">
+        <a className="navbar-brand text-white" href="/">Users Data</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Table.js
@@ -0,0 +1,144 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Table = () => {
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [detailsOpen, setDetailsOpen] = useState(false);
+  const [selectedUser, setSelectedUser] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [error, setError] = useState(null); // New state for handling errors
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/api/data'); 
+        setData(response.data);
+        setFilteredData(response.data);
+        setLoading(false);
+      } catch (err) {
+        console.error(err);
+        setError('Error fetching data. Please try again later.');
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (searchQuery.trim() === '') {
+      setFilteredData(data);
+    } else {
+      const filtered = data.filter((user) =>
+        user.username.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      setFilteredData(filtered);
+    }
+  }, [searchQuery, data]);
+
+  const openDetails = (user) => {
+    setSelectedUser(user);
+    setDetailsOpen(true);
+  };
+
+  const closeDetails = () => {
+    setSelectedUser({});
+    setDetailsOpen(false);
+  };
+
+  return (
+    <div className="container mt-4">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search by Username..."
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button className="btn btn-primary" type="button">
+              Search
+            </button>
+          </div>
+          {error ? (
+            <div className="alert alert-danger" role="alert">
+              {error}
+            </div>
+          ) : (
+            <div className="table-responsive" style={{ width: '100%' }}>
+              <table className="table table-striped">
+                <thead>
+                  <tr>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Company Name</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {filteredData.map((user) => (
+                    <tr key={user.id}>
+                      <td>{user.username}</td>
+                      <td>{user.email}</td>
+                      <td>{user.phone}</td>
+                      <td>{user.company.name}</td>
+                      <td>
+                        <button
+                          className="btn btn-primary"
+                          onClick={() => openDetails(user)}
+                        >
+                          Details
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div
+        className={`modal fade ${detailsOpen ? 'show' : ''}`}
+        style={{ display: detailsOpen ? 'block' : 'none' }}
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">User Details</h5>
+              <button
+                type="button"
+                className="btn-close"
+                onClick={closeDetails}
+              ></button>
+            </div>
+            <div className="modal-body">
+              {detailsOpen ? (
+                <div>
+                  <div className="user-details-popup">
+                    <p>Name: {selectedUser.name}</p>
+                    <p>Username: {selectedUser.username}</p>
+                    <p>Email: {selectedUser.email}</p>
+                    <p>Phone: {selectedUser.phone}</p>
+                    <p>Website: {selectedUser.website}</p>
+                    <p>Company Name: {selectedUser.company.name}</p>
+                    <p>
+                      Address: {selectedUser.address.street}, {selectedUser.address.city}, {selectedUser.address.zipcode}
+                    </p>
+                  </div>
+                </div>
+              ) : null}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Table;
